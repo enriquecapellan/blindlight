0d6852c9c8ed3f71b933a38dcd62cb49db9efbc2
Enrique Capellan <enriqcapellan@gmail.com>
2021-10-30 16:47:19 -0400

    send bytes

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI, File
 from utils import extract_labels
 import base64
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    image: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.post("/image_labels")
-async def image_labels(image: str = ''):
-    message_bytes = image.encode('ascii')
-    base64_bytes = base64.b64encode(message_bytes)
-    finalImage = base64_bytes.decode('ascii')
+async def image_labels(model: Item):
+    message_bytes = base64.b64decode(model.image+ "==")
 
-    labels = extract_labels(base64_bytes)
+
+    labels = extract_labels(message_bytes)
     return labels
